15988bb3803a62ce203060fc019953087baf53b7
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 <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>
2023-02-23 01:14:36 +0300

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
- * main - print _putchar
- * is written by Lewis 
- * Return: 0
- */
+ ** main - print _putchar
+ ** is written by mbah
+ ** Return: 0
+ **/
+
 
 int main(void)
 {
-_putchar('-');
+_putchar('_');
 _putchar('p');
 _putchar('u');
 _putchar('t');
@@ -15,6 +16,6 @@ _putchar('c');
 _putchar('h');
 _putchar('a');
 _putchar('r');
-_putchar('\n'};
+_putchar('\n');
 return (0);
 }
